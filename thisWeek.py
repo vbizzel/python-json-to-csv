ff4bfd32fbfa8c6baeb1e765758669f40cad5e1b
vbizzel <j.vbussche@gmail.com>
2015-06-17 13:21:20 -0400

    Update thisWeek.py

--- thisWeek.py
+++ thisWeek.py
@@ -2,12 +2,12 @@ import requests    # module that makes http requests
 import json # module for JavaScript Object Notation
 import csv
 
-# this http request to JIRA will get the IR and by the date it was created. This one fetches all IR's created in the last 7 days.
+# this http request to JIRA will get the IR and by the date it was created. This one fetches all IR's created in the last week.
 this_week = requests.get('https://domain.com/rest/api/2/search?jql=project=IR%20and%20created%20%3E=%20startOfWeek(-0w)', auth=('username', 'password'))    #access to the url, needs username and password to JIRA
 
 def thisWeek():
 	
-	with open('/Users/directory/thisWeek.txt', 'w+') as fileOut:   # dumps the information into a text file on local machine
+	with open('/Users/directory/Desktop/Python_OutPut/thisWeek.txt', 'w+') as fileOut:   # dumps the information into a text file on local machine
 		fileOut.write(json.dumps(this_week.json(), indent = 4 ))  # prints the API JSON request informaton with formatting in python
 		fileOut.close()
 
@@ -37,7 +37,7 @@ def list_info():   #function containing the search fields and appending them to 
 
 def thisWeekCSV(): # function that opens up a csv file to put the filtered data into
 
-	with open('/Users/directory/thisWeek.csv', 'w+') as csvFileOut:  #prints the information from list_info into a csv file
+	with open('/Users/directory/Desktop/Python_OutPut/thisWeek.csv', 'w+') as csvFileOut:  #prints the information from list_info into a csv file
 		wr = csv.writer(csvFileOut, quoting=csv.QUOTE_ALL)
 		for row in list_info():
 			wr.writerow([w.encode('utf-8') for w in row])
