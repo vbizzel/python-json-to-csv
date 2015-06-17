82504a44b3542a36702a8fe509d00e9b61a6f34a
vbizzel <j.vbussche@gmail.com>
2015-06-17 13:31:23 -0400

    Update jan2015_current.py

--- jan2015_current.py
+++ jan2015_current.py
@@ -2,11 +2,11 @@ import requests    # module that makes http requests
 import json # module for JavaScript Object Notation
 import csv
 
-jan2015_current = requests.get('https://domain.com/rest/api/2/search?jql=project=IR%20and%20created%20%3E=%20startOfDay(-7d)', auth=('username', 'password'))
+jan2015_current = requests.get('https://domain.com/rest/api/2/search?jql=project%20=%20IR%20AND%20created%20%3E=%202015-01-01%20AND%20created%20%3C=%20now()%20&maxResults=1000', auth=('username', 'password'))
 
 def janToCurrent():
 	
-	with open('/Users/directory/jan2015_current.txt', 'w+') as fileOut:   # dumps the information into a text file on local machine
+	with open('/Users/directory/Desktop/Python_OutPut/jan2015_current.txt', 'w+') as fileOut:   # dumps the information into a text file on local machine
 		fileOut.write(json.dumps(jan2015_current.json(), indent = 4 ))  # prints the API JSON request informaton with formatting in python
 		fileOut.close()
 
@@ -36,7 +36,7 @@ def list_info():   #function containing the search fields and appending them to 
 
 def janToCurrentCSV(): # function that opens up a csv file to put the filtered data into
 
-	with open('/Users/directory/jan2015_current.csv', 'w+') as csvFileOut:  #prints the information from list_info into a csv file
+	with open('/Users/directory/Desktop/Python_OutPut/jan2015_current.csv', 'w+') as csvFileOut:  #prints the information from list_info into a csv file
 		wr = csv.writer(csvFileOut, quoting=csv.QUOTE_ALL)
 		for row in list_info():
 			wr.writerow([w.encode('utf-8') for w in row])
