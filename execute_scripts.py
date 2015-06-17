98f0994ef05e9576c5eceabbacb999b243cd2345
vbizzel <j.vbussche@gmail.com>
2015-06-17 13:35:52 -0400

    Create execute_scripts.py

---
+++ execute_scripts.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#this script executes all the scripts one after another.
+execfile("thisWeek.py"),
+execfile("lastWeek.py"), 
+execfile("thisMonth.py"), 
+execfile("lastMonth.py"), 
+execfile("jan2015_current.py"), 
+execfile("july2014_dec2014.py") 
+
